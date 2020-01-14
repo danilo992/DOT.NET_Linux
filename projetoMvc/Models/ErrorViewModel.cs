72cbe5e8b05bf95891fcdd58daaea003a357b793
danilo992 <tadeudanilo46@gmail.com>
2020-01-14 01:02:34 -0300

    iniciando projeto MVC

---
+++ projetoMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projetoMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ projeto_console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DOT.NET_linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 29;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
